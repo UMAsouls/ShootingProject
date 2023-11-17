bbb2b8bda9064f525ba322f1c34b16717af8777a
UMAsouls <umaplay972@gmail.com>
2023-11-17 13:23:01 +0900

    [Add] Test program and data

---
+++ src/object/TestBG.py
@@ -0,0 +1,34 @@
+import pygame
+from pygame import locals
+import os
+
+os.chdir("..")
+
+from IGroups import IGroups
+
+from GameObject import GameObject
+from Vector import Vector
+
+from Dependencybuillder import Dependency
+from Drawer import Drawer
+
+groups : IGroups = Dependency[IGroups]()
+
+class TestBG(GameObject):
+    def __init__(self):
+        super().__init__()
+        
+        Drawer.objects.change_layer(self, 0)
+        
+        self.time = 0
+        
+    def update(self):
+        super().update()
+        if(self.time < 0):
+            self.moving = False
+        else:
+            self.time -= 1
+        test_sp : GameObject = groups.get_single("test2")
+        
+        #print(test_sp.name)
+    
