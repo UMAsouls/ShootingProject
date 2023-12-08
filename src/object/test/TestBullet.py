cedf150053db053e63e91118f3a22ff5c789cc39
Riku Uruma <riku.uruma.2c@stu.hosei.ac.jp>
2023-12-08 14:57:11 +0900

    当たり判定追加しました

--- src/object/test/TestBullet.py
+++ src/object/test/TestBullet.py
@@ -11,17 +11,16 @@ class TestBullet(GameObject):
         super().set_data(data)
         
         self.vel = Vector(1,1)
+        self.set_velocity(10,90)
 
     def set_velocity(self, speed, angle):
         x = speed * math.cos(math.radians(angle))
         y = speed * math.sin(math.radians(angle))
         self.vel = Vector(x,y)
-        print(x,y)
     
     
         
     def update(self):
         super().update()
-        self.set_velocity(10,90)
 
         self._position += self.vel
