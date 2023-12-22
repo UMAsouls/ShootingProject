33df0f660f94a9fcd5d385d92562e19375cef9ee
wasabi7821 <sori.ota.4x@stu.hosei.ac.jp>
2023-12-22 14:11:13 +0900

    投げる間隔

--- src/object/test/TestMachine.py
+++ src/object/test/TestMachine.py
@@ -13,6 +13,7 @@ class TestMachine(GameObject):
         super().set_data(data)
         
         self.vel = Vector(0,0)
+        self.interval = 0
 
         self.ball = data["ball_data"]
         self.speed = data["speed"]
@@ -36,18 +37,26 @@ class TestMachine(GameObject):
             self.vel += Vector(0,-1*self.speed)
         if(self._key.get_key_repeat("s")):
             self.vel += Vector(0,self.speed)
-            
-        if(self._key.get_key_down("c")):
-            self.shoot(1)
 
-        if(self._key.get_key_down("v")):
-            self.shoot(2)
+        if self.interval == 25:
+            if(self._key.get_key_down("c")):
+                self.shoot(1)
+                self.interval = 0
 
-        if(self._key.get_key_down("m")):
-            self.shoot(3)
+            if(self._key.get_key_down("v")):
+                self.shoot(2)
+                self.interval = 0
 
-        if(self._key.get_key_down("n")):
-            self.shoot(4)
-            
+            if(self._key.get_key_down("m")):
+                self.shoot(3)
+                self.interval = 0
+
+            if(self._key.get_key_down("n")):
+                self.shoot(4)
+                self.interval = 0
+
+        else:
+            self.interval += 1
+           
         self.position += self.vel
             
