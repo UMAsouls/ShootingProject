fe1e5bb8f2229dcbdac8786fb2dc6773191df544
UMAsouls <umaplay972@gmail.com>
2024-01-06 15:22:17 +0900

    timer change

--- object/shooting/Counter.py
+++ object/shooting/Counter.py
@@ -1,12 +1,18 @@
+import pygame
+
 from pygameEasy.GameObject import TextObject
 
 class Counter(TextObject):
     def set_data(self, data) -> None:
         super().set_data(data)
         
+        
+        self.clock = pygame.time.Clock()
+        
         self.count = data["count"]
         
     def update(self) -> None:
-        self.text = f"{self.count}"
+        self.clock.tick()
+        self.text = f"{self.count}"[:5]
         
-        self.count -= 1
+        self.count -= self.clock.get_rawtime() / 1000
