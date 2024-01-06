56476ad679d1ca677c45e33cb0d569de7da26151
UMAsouls <umaplay972@gmail.com>
2024-01-06 15:35:42 +0900

    background size

--- object/shooting/Background.py
+++ object/shooting/Background.py
@@ -1,9 +1,14 @@
+import pygame
 
 from pygameEasy.GameObject import GameObject
 
 class Background(GameObject):
     def set_data(self, data):
         super().set_data(data)
+        
+        size = pygame.display.get_surface().get_size()
+        
+        self.size = size
         
     def update(self):
         super().update()
